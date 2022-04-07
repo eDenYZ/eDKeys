3b8386566b276955dbaea255f5d3be25aa9a0d7b
eDen <85058066+eDenYZ@users.noreply.github.com>
2022-04-07 12:47:51 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -26,47 +26,48 @@ end)
 
 RegisterServerEvent("</eDen:donnerkey")
 AddEventHandler("</eDen:donnerkey", function(target, plate)
-	if (not (target ~= nil)) then
-		return
-	end
-	if (not (plate ~= nil)) then
-		return
-	end
-	local src = source
-	local xPlayer = ESX.GetPlayerFromId(src)
-	local xTarget = ESX.GetPlayerFromId(target)
-	local toplate = plate
-	if (xPlayer) then
-		MySQL.Async.fetchAll("SELECT * FROM open_car WHERE identifier = @identifier", {
-			["@identifier"] = xPlayer.identifier
-		}, function(result)
-			if (result) then
-				for k, v in pairs(result) do
-					if (v.value == toplate) then
-						MySQL.Async.execute("UPDATE open_car SET identifier = @identifier WHERE value = @value", {
-							["@identifier"] = xTarget.identifier,
-							["@value"] = toplate
-						})
-					end
-				end
-			end
-		end)
-		MySQL.Async.fetchAll("SELECT * FROM owned_vehicles WHERE owner = @owner", {
-			["@owner"] = xPlayer.identifier
-		}, function(result)
-			if (result) then
-				for k, v in pairs(result) do
-					if (v.plate == toplate) then
-						MySQL.Async.execute("UPDATE owned_vehicles SET owner = @owner WHERE plate = @plate", {
-							["@owner"] = xTarget.identifier,
-							["@plate"] = toplate
-						})
-						print("Insert Into Terminé")
-					end
-				end
-			end
-		end)
-		TriggerClientEvent("esx:showNotification", xTarget.source, "Vous avez reçu de nouvelle clé ")
-		TriggerClientEvent("esx:showNotification", xPlayer.source, "Vous avez donné votre clé, vous ne les avez plus !")
-	end
+    if (not (target ~= nil)) then
+        return
+    end
+    if (not (plate ~= nil)) then
+        return
+    end
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    local xTarget = ESX.GetPlayerFromId(target)
+    local toplate = plate
+    if (xPlayer) then
+        MySQL.Async.fetchAll("SELECT * FROM open_car WHERE identifier = @identifier", {
+            ["@identifier"] = xPlayer.identifier
+        }, function(result)
+            if (result) then
+                for k, v in pairs(result) do
+                    if (v.value == toplate) then
+                        MySQL.Async.execute("UPDATE open_car SET identifier = @identifier WHERE value = @value", {
+                            ["@identifier"] = xTarget.identifier,
+                            ["@value"] = toplate
+                        })
+                        print("UPDATE Terminé")
+                    end
+                end
+            end
+        end)
+        MySQL.Async.fetchAll("SELECT * FROM owned_vehicles WHERE owner = @owner", {
+            ["@owner"] = xPlayer.identifier
+        }, function(result)
+            if (result) then
+                for k, v in pairs(result) do
+                    if (v.plate == toplate) then
+                        MySQL.Async.execute("UPDATE owned_vehicles SET owner = @owner WHERE plate = @plate", {
+                            ["@owner"] = xTarget.identifier,
+                            ["@plate"] = toplate
+                        })
+                        print("UPDATE Terminé")
+                    end
+                end
+            end
+        end)
+        TriggerClientEvent("esx:showNotification", xTarget.source, "Vous avez reçu de nouvelle clé ")
+        TriggerClientEvent("esx:showNotification", xPlayer.source, "Vous avez donné votre clé, vous ne les avez plus !")
+    end
 end)
